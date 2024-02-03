199345425cefb64d07b1902bcd080f6815b6feb9
Hugo Leijonhufvud <thechosenwolf@outlook.com>
2024-02-03 20:06:56 +0100

    AddGenres method completed

--- TbdMinimalMusicAPi/Repositories/TbdRepository.cs
+++ TbdMinimalMusicAPi/Repositories/TbdRepository.cs
@@ -35,10 +35,29 @@ namespace TbdMinimalMusicAPi.Repositories
                 Console.WriteLine($"Error adding artists: {ex}");
             }
         }
-
-        public void AddGenres(List<Genre> genres, int userId, int artistId)
+        // This method adds genres to a specific user. 
+        public void AddGenres(List<Genre> genresToAdd, int userId, int artistId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var user = _context.Users
+                    .Include(u => u.Artists)
+                    .Include(u => u.Genres)
+                    .FirstOrDefault(u => u.UserId == userId);
+                var artist = _context.Artists
+                    .Include(u => u.Genres)
+                    .FirstOrDefault(u => u.ArtistId == artistId);
+                foreach (var genreToAdd in genresToAdd)
+                {
+                    user.Genres.Add(genreToAdd);
+                    artist.Genres.Add(genreToAdd);
+                }
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error adding artists: {ex}");
+            }
         }
 
         public void AddSongs(List<Song> songs, int artistId, int userId, int genreId)
