03f002f50c7111d4111c61f707a3c2a1d61b2a56
dinozoff1981 <tensaeg@yahoo.com>
2024-02-03 19:48:49 +0100

    Revert "Irepository creation completed"
    This reverts commit af1a750c7019208fab3852c26bf19fac274d1af1.

--- TbdMinimalMusicAPi/Program.cs
+++ TbdMinimalMusicAPi/Program.cs
@@ -1,7 +1,5 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TbdMinimalMusicAPi.Data;
-using TbdMinimalMusicAPi.Repositories;
 
 namespace TbdMinimalMusicAPi
 {
@@ -12,9 +10,9 @@ namespace TbdMinimalMusicAPi
             var builder = WebApplication.CreateBuilder(args);
             var connectionstring = builder.Configuration.GetConnectionString("TbdDb");
             builder.Services.AddDbContext<TbdContext>(option => option.UseSqlServer(connectionstring));
-            builder.Services.AddScoped<ItbdRepositori, TbdRepositori>();
             var app = builder.Build();
 
+          
 
             app.Run();
         }
